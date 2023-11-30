2264d64fcc15d56bddadf87691457d1773767613
Onurcan Kurt <onurcankurt96@gmail.com>
2023-11-30 17:47:21 +0300

    Initial Commit

---
+++ HeroWidget/HeroWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroWidgetApp.swift
+//  HeroWidget
+//
+//  Created by onur on 30.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct HeroWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
