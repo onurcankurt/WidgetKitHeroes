052408452c8fe8ea5c98ebad1ff90b4920539afc
Onurcan Kurt <onurcankurt96@gmail.com>
2023-12-01 17:22:13 +0300

    AppStorage added

--- HeroWidget/ContentView.swift
+++ HeroWidget/ContentView.swift
@@ -10,13 +10,28 @@ import SwiftUI
 let superHeroArray = [spiderman, hulk, deadpool, batman, wolverine]
 
 struct ContentView: View {
+    
+    @AppStorage("hero", store: UserDefaults(suiteName: "group.com.onurcankurt.HeroWidget"))
+    var heroData : Data = Data()
+    
     var body: some View {
         VStack {
             ForEach(superHeroArray){hero in
                 HeroView(hero: hero)
+                    .onTapGesture {
+                        saveToDefaults(hero: hero)
+                }
             }
         }
         .padding()
+    }
+    
+    func saveToDefaults(hero: Superhero) {
+        //print(hero.name)
+        if let heroData = try? JSONEncoder().encode(hero) {
+            self.heroData = heroData
+            print(hero.name)
+        }  
     }
 }
 
