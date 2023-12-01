d69867ddff5ac33057b693b449f5d8ac044335c2
Onurcan Kurt <onurcankurt96@gmail.com>
2023-12-01 16:29:18 +0300

    CircularImageView codes added

--- HeroWidget/CircularImageView.swift
+++ HeroWidget/CircularImageView.swift
@@ -8,11 +8,17 @@
 import SwiftUI
 
 struct CircularImageView: View {
+    
+    var image : Image
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        image.resizable()
+            .aspectRatio(contentMode: .fit)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.gray, lineWidth: 5))
+            .shadow(radius: 15)
     }
 }
 
 #Preview {
-    CircularImageView()
+    CircularImageView(image: Image("spiderman"))
 }
