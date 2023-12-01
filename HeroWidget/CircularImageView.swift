d87be24a0eb7d3ed7ea208e6ecfdb03a9af219eb
Onurcan Kurt <onurcankurt96@gmail.com>
2023-12-01 16:28:42 +0300

    CircularImageView added

---
+++ HeroWidget/CircularImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularImageView.swift
+//  HeroWidget
+//
+//  Created by onur on 30.11.2023.
+//
+
+import SwiftUI
+
+struct CircularImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularImageView()
+}
